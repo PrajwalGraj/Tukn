31f4762cab0febf5d6d5596e45e6e09483291f8e
Prajwal g <prajwalg@Prajwals-MacBook-Air.local>
2025-10-28 20:23:55 +0530

    Fix: removed invalid export from route file

--- app/api/actions/token/create/route.ts
+++ app/api/actions/token/create/route.ts
@@ -29,7 +29,7 @@ import {
 } from "@metaplex-foundation/mpl-token-metadata";
 
 //Metaplex Token Metadata Program ID
-export const MPL_TOKEN_METADATA_PROGRAM_ID = new PublicKey(
+const MPL_TOKEN_METADATA_PROGRAM_ID = new PublicKey(
   "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
 );
 
